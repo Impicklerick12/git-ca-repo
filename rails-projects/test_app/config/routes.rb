05b3eca7d32d16ae93d510a3e4b168314e7cb7db
Tyler Hall <tylerhall12@outlook.com>
2020-06-22 14:23:49 +1000

    Updates to Rails apps

--- rails-projects/test_app/config/routes.rb
+++ rails-projects/test_app/config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
+  get "/movies", to: "movies#index", as: "movies"
+  post "/movies", to: "movies#create"
+  get "/movies/new", to: "movies#new", as: "new_movie"
+  get "/movies/:id", to: "movies#show", as:"movie"
+  put "/movies/:id", to: "movies#update"
+  patch "/movies/:id", to: "movies#update"
+  get "/movies/:id/edit", to: "movies#edit", as:"edit_movie"
+  
   # Home/landing page
-  get "/", to: "home#index", as: "root"
+  root "home#index"
 
   # Celebrities
   get "/celebrities/index", to: "celebrities#index", as: "celebrities_index"
